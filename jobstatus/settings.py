b0c726afefd34f63c50b566760a4264fa898fde6
Koushik Naskar <koushik.naskar9@gmail.com>
2021-11-23 00:02:17 +0530

    log in debug mode

--- jobstatus/settings.py
+++ jobstatus/settings.py
@@ -15,7 +15,9 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from django.utils.log import DEFAULT_LOGGING
 
+DEFAULT_LOGGING['handlers']['console']['filters'] = []
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
